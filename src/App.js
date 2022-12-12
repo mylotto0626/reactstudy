0ad3193712697f6787ec6dab2cd4ac739f671211
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-12-12 11:43:52 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>WEB</h1>
+        <nav>
+          <ul>
+            <li><a href="/read/1">html</a></li>
+            <li><a href="/read/2">css</a></li>
+            <li><a href="/read/3">js</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
