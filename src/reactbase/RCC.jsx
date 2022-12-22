f7398fe0919f37b34400bdafa4a245ea6d858829
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-12-22 14:57:19 +0900

    REACT - Reactjs Code Snippet

---
+++ src/reactbase/RCC.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RCC extends Component {
+  render() {
+    return (
+      <div>RCC</div>
+    )
+  }
+}
---
+++ src/reactbase/js_string.js
@@ -0,0 +1,20 @@
+let str1='안녕' 
+let str2='반가워'
+let hello=str1+str2; //병합 연산자를 통해 문자열과 문자열을 연결
+let object={name:"도서",price:4300};
+
+//문자열과 변수를 연결할 때도 병합 연산자를 사용
+let message='제품 '+object.name+"의 가격은 "+object.price+"입니다"; 
+let num1=3;
+let num2=5;
+let mul="두 수의 곱셈 값은 "+(num1*num2)+"입니다"; //연산 결과를 괄호로 묶어 연산 구문을 먼저 실행
+
+console.log(str1)
+console.log(str2)
+console.log(hello)
+console.log(object)
+console.log(message)
+console.log(num1)
+console.log(num2)
+console.log(mul);
+
